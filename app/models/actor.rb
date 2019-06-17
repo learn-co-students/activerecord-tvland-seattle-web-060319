9843dcb010eb50af28b2a69c430e88eff2087602
Claire Wilbert <claire.wilbert@gmail.com>
2019-06-17 16:19:07 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end 
+
+# strings in the form "#{character_name} - #{show_name}"
+  def list_roles
+    self.characters.map do |char|
+      "#{char.name} - #{char.show.name}"
+    end
+  end
+
 end
+
