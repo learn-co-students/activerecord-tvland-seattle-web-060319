51364946701b125d7b37732acc67a2000e44d6e2
Spencer Lindemuth <lindemut@gmail.com>
2019-06-17 15:15:51 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
+    def list_roles
+        concat_roles(self.characters)
+    end
+
+
+    def concat_roles(array)
+        array.map do |char|
+            "#{char.name} - #{char.show.name}"
+        end
+    end
+
+
+
+
 end
