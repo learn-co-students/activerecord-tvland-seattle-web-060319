e2fd599cd5f4c570bd987c511dfa2ea3f3e1adf3
Karen Go <karenrgo@gmail.com>
2019-06-21 11:06:53 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    my_characters = Character.where(actor_id: self.id)
+    roles = []
+    my_characters.each do |ch|
+        roles << "#{ch.name} - #{Show.where(id: ch.show_id).first.name}"
+    end
+    roles
+  end
 end
